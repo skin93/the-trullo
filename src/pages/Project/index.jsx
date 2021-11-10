f1f4d84529b73bbe3052213665ebb78d714f278e
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-11-10 20:35:49 +0100

    Navigate to specific document

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,5 +1,19 @@
+import { useParams } from 'react-router-dom';
+import { useDocument } from 'hooks/useDocument';
+import { Error } from 'styles/GlobalStyle';
+
 const Project = () => {
-  return <div>Project Page</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument('projects', id);
+
+  if (error) return <Error>{error}</Error>;
+  if (!document) return <div>Loading...</div>;
+
+  return (
+    <div className='project-details'>
+      <h1>{document.name}</h1>
+    </div>
+  );
 };
 
 export default Project;
